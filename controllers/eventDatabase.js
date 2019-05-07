6985af3f532f67ff7834e86330c88defeb6f1df1
unknown <lashleykeith@gmail.com>
2019-05-07 22:02:07 +0900

    this is the first go at the user admin backend

--- controllers/eventDatabase.js
+++ controllers/eventDatabase.js
@@ -2,8 +2,43 @@
  * GET /
  * Home page.
  */
+const Event = require('../models/Event');
 exports.getEventDatabase = (req, res) => {
-  res.render('eventDatabase', {
-    title: 'eventDatabase'
+
+   // get all events   
+   Event.find({}, (err, events) => {
+    if (err) {
+      res.status(404);
+      res.send('Events not found!');
+    }
+    // return a view with data
+    res.render('eventDatabase', { 
+      title: 'eventDatabase',
+      events: events,
+      success: req.flash('success')
+    });
   });
+ 
+  // res.render('eventDatabase', {
+  //   title: 'eventDatabase'
+  // });
 };
+
+/**
+ * Show all events
+ */
+function showEvents(req, res) {
+  // get all events   
+  Event.find({}, (err, events) => {
+    if (err) {
+      res.status(404);
+      res.send('Events not found!');
+    }
+
+    // return a view with data
+    res.render('pages/events', { 
+      events: events,
+      success: req.flash('success')
+    });
+  });
+}
