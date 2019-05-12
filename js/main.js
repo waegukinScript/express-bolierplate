00def7fe382ba43c359fe96d7b60b5e94709a842
unknown <lashleykeith@gmail.com>
2019-05-12 20:19:18 +0900

    footer is commented out but everything else is fine.  Useradmin still not completed.

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ $(document).ready(function () {
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: '/contact', // form action url
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
