776bd1861944fabe130f2377532142d98c02f196
unknown <lashleykeith@gmail.com>
2019-05-16 22:51:40 +0900

    user admin can delete, edit and update  now next we need to make an email functionality

--- controllers/event.js
+++ controllers/event.js
@@ -1,20 +1,9 @@
-// const nodemailer = require("nodemailer");
 const Event = require('../models/Event');
 
-/**
- * POST /event
- */
-exports.postEvent = (req, res) => {
-
+exports.postEvent = (req, res, next) => {
   const obj = req.body;
-
-  //Here, I have set default value
-  //obj.eventTime = "1";
-  //obj.guest = "1";
-
-  console.log("Events");
+  console.log('Events');
   console.log(obj);
-
   const event = new Event({
     eventName: obj.eventName,
     eventDate: obj.eventDate,
@@ -26,52 +15,61 @@ exports.postEvent = (req, res) => {
     phoneNumber: obj.phoneNumber,
     postMessage: obj.postMessage,
   });
-
   event.save((err) => {
     if (err) {
       return next(err);
     }
     res.redirect('/');
   });
-
-};
-// Deletes the post from the admin
-exports.postDeleteEvent =  (req, res, next) => {
-  const {id} = (req.params)
-  doc =   Event.findOneAndDelete({_id: id},function(err, doc) {
-    if( err) {
-      console.log(err);
-    }
-    console.log(doc)
-    return res.redirect('back')
-  })
-  console.log("message blah after button clicked", req.event)
-  
 };
 
-// 1.Trying to edit the form
-exports.postEditEvent = (req, res, next) => {
-  const {id} = (req.params)
-  doc = Event.findById({_id: id}, function(err, doc){
+exports.postDeleteEvent = (req, res, next) => {
+  const { id } = (req.params);
+  Event.findOneAndDelete({ _id: id }, (err) => {
     if (err) {
       console.log(err);
+    } else {
+      return res.redirect('back');
     }
-    console.log(doc)
-    return res.redirect('back')
-  })
-  console.log("you are editing this row", req.event)
+  });
 };
 
-//2. Then we are going to try to update the form
+exports.postEditEvent = (req, res, next) => {
+  const { id } = (req.params);
+  Event.findById({ _id: id }, (err, event) => {
+    if (err) {
+      console.log(err);
+    } else if (event) {
+      res.render('editEvent', {
+        title: 'Edit Event',
+        event
+      });
+    }
+  });
+};
 
 exports.postUpdateEvent = (req, res, next) => {
-  const {id} = (req.params)
-  doc = Event.findById({_id: id}, function(err, doc){
+  const { id } = (req.params);
+  const obj = req.body;
+  Event.findById({ _id: id }, (err, event) => {
     if (err) {
       console.log(err);
+    } else if (event) {
+      event.eventName = obj.eventName;
+      event.eventDate = obj.eventDate;
+      event.eventTime = obj.eventTime;
+      event.guest = obj.guest;
+      event.budget = obj.budget;
+      event.contactName = obj.contactName;
+      event.emailAddress = obj.emailAddress;
+      event.phoneNumber = obj.phoneNumber;
+      event.postMessage = obj.postMessage;
+      event.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect('/eventDatabase');
+      });
     }
-    console.log(doc)
-    return res.redirect('back')
-  })
-  console.log("you are editing this row", req.event)
+  });
 };
